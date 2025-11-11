bf49753fd8ad76e56e4e6c45a34d4bee1b54e92e
Zainab Salman aka Zubiii :) <zubithecatcake@gmail.com>
2025-11-11 17:07:16 +0500

    Create basic_flask_code.py

---
+++ basic_flask_code.py
@@ -0,0 +1,10 @@
+# Basic Flask Code
+
+from practice_flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return jsonify(message="Hello, World!") 
+if __name__ == '__main__':
+    app.run(debug=True)     # Run the App
+
