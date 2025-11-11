0317a81350a9148a89f993eddad4d09fb16f7bfb
Zainab Salman aka Zubiii :) <zubithecatcake@gmail.com>
2025-11-11 21:16:18 +0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {"id": self.id, "name": self.name, "age": self.age}
